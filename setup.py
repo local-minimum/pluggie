c1979dad77da6c75656e1d8a9d4380cc359ca916
Martin Zackrisson <martin.zackrisson@fripost.org>
2018-10-06 14:31:01 +0200

    Adding a failing tox

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="Pluggie",
+    version="0.0.1",
+    description="A simple to use and code for plugin system",
+    author="Martin Zackrisson",
+    packages=["pluggie"],
+)
